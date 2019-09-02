b8e6ef694af7b9d665f7cb2b7abfba3144b3d1da
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-02 14:51:37 +0900

    Modified Layout

--- web/components/Layout/index.js
+++ web/components/Layout/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Head from 'next/head';
 import * as S from './styles';
 
 import Header from 'components/Header';
 
 export default function Layout({ children }) {
   return (
-    <S.Layout>
-      <Header />
-      <section>{children}</section>
-    </S.Layout>
+    <React.Fragment>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800&display=swap&subset=korean"
+          rel="stylesheet"
+        />
+      </Head>
+      <S.Layout>
+        <Header />
+        <section>
+          <S.Container>{children}</S.Container>
+        </section>
+      </S.Layout>
+    </React.Fragment>
   );
 }
--- web/components/Layout/styles.js
+++ web/components/Layout/styles.js
@@ -1,3 +1,8 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div``;
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0 2rem;
+`;
