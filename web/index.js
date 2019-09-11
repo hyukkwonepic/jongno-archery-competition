8e503007487682ad937989ed415eaa9ff6d4b3c4
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-11 10:34:31 +0900

    Add firebase function settings for web

--- web/index.js
+++ web/index.js
@@ -1,24 +1,14 @@
-// const admin = require('firebase-admin');
-// const functions = require('firebase-functions');
-// const next = require('next');
+const functions = require('firebase-functions');
+const next = require('next');
+const express = require('express');
 
-// admin.initializeApp();
+const app = next({ dir: __dirname, conf: { distDir: 'next' } });
+const handle = app.getRequestHandler();
 
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({
-//   dev,
-//   // the absolute directory from the package.json file that initialises this module
-//   // IE: the absolute path from the root of the Cloud Function
-//   conf: { distDir: 'dist/' }
-// });
-// const handle = app.getRequestHandler();
+const server = express();
 
-// const web = functions.https.onRequest((request, response) => {
-//   // log the page.js file or resource being requested
-//   console.log('File: ' + request.originalUrl);
-//   return app.prepare().then(() => handle(request, response));
-// });
+server.get('*', (req, res) => {
+  return handle(req, res);
+});
 
-// module.exports = {
-//   web
-// };
+exports.web = functions.https.onRequest(server);
