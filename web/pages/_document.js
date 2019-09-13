bf41f1ccf752d27247d297eb154b286ff139a1c0
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-14 07:58:02 +0900

    Modify document to ssr material ui's styling rules

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Document from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets as MaterialSheet } from '@material-ui/styles';
+import { ServerStyleSheet as StyledComponentSheet } from 'styled-components';
 
 import GlobalStyle from '../components/GlobalStyle';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
+    const materialSheet = new MaterialSheet();
+    const styledComponentsSheet = new StyledComponentSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: App => props =>
-            sheet.collectStyles(
-              <React.Fragment>
-                <GlobalStyle />
-                <App {...props} />
-              </React.Fragment>
+            materialSheet.collect(
+              styledComponentsSheet.collectStyles(
+                <React.Fragment>
+                  <CssBaseline />
+                  <GlobalStyle />
+                  <App {...props} />
+                </React.Fragment>
+              )
             )
         });
 
@@ -27,12 +33,13 @@ export default class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
+            {materialSheet.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
           </>
         )
       };
     } finally {
-      sheet.seal();
+      styledComponentsSheet.seal();
     }
   }
 }
