b152e8523d612cba9fc3c0686e5446eb537638fb
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-14 07:57:26 +0900

    Modified apollo for user authentication

--- web/lib/apollo.js
+++ web/lib/apollo.js
@@ -1,12 +1,13 @@
 import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import cookie from 'cookie';
 import Head from 'next/head';
-import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'isomorphic-unfetch';
-
-let apolloClient = null;
 
 /**
  * Creates and provides the apolloContext
@@ -18,10 +19,15 @@ let apolloClient = null;
  */
 export function withApollo(PageComponent, { ssr = true } = {}) {
   const WithApollo = ({ apolloClient, apolloState, ...pageProps }) => {
-    const client = useMemo(
-      () => apolloClient || initApolloClient(apolloState),
-      []
-    );
+    const client = useMemo(() => {
+      // We pass in the apolloClient directly when using getDataFromTree
+      if (apolloClient) {
+        return apolloClient;
+      }
+
+      // Otherwise initClient using apolloState
+      return initApolloClient(apolloState, { getSession });
+    }, []);
     return (
       <ApolloProvider client={client}>
         <PageComponent {...pageProps} />
@@ -29,41 +35,53 @@ export function withApollo(PageComponent, { ssr = true } = {}) {
     );
   };
 
-  // Set the correct displayName in development
   if (process.env.NODE_ENV !== 'production') {
+    // Find correct display name
     const displayName =
       PageComponent.displayName || PageComponent.name || 'Component';
 
+    // Warn if old way of installing apollo is used
     if (displayName === 'App') {
       console.warn('This withApollo HOC only works with PageComponents.');
     }
 
+    // Set correct display name for devtools
     WithApollo.displayName = `withApollo(${displayName})`;
+
+    // Add some prop types
+    WithApollo.propTypes = {
+      // Used for getDataFromTree rendering
+      apolloClient: PropTypes.object,
+      // Used for client/server rendering
+      apolloState: PropTypes.object
+    };
   }
 
   if (ssr || PageComponent.getInitialProps) {
     WithApollo.getInitialProps = async ctx => {
       const { AppTree } = ctx;
 
-      // Initialize ApolloClient, add it to the ctx object so
-      // we can use it in `PageComponent.getInitialProp`.
-      const apolloClient = (ctx.apolloClient = initApolloClient());
+      // Run all GraphQL queries in the component tree
+      // and extract the resulting data
+      const apolloClient = (ctx.apolloClient = initApolloClient(
+        {},
+        {
+          getSession: () => getSession(ctx.req)
+        }
+      ));
 
-      // Run wrapped getInitialProps methods
-      let pageProps = {};
-      if (PageComponent.getInitialProps) {
-        pageProps = await PageComponent.getInitialProps(ctx);
-      }
+      const pageProps = PageComponent.getInitialProps
+        ? await PageComponent.getInitialProps(ctx)
+        : {};
 
-      // Only on the server:
+      // Only on the server
       if (typeof window === 'undefined') {
         // When redirecting, the response is finished.
         // No point in continuing to render
         if (ctx.res && ctx.res.finished) {
-          return pageProps;
+          return {};
         }
 
-        // Only if ssr is enabled
         if (ssr) {
           try {
             // Run all GraphQL queries
@@ -82,11 +100,11 @@ export function withApollo(PageComponent, { ssr = true } = {}) {
             // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
             console.error('Error while running `getDataFromTree`', error);
           }
-
-          // getDataFromTree does not call componentWillUnmount
-          // head side effect therefore need to be cleared manually
-          Head.rewind();
         }
+
+        // getDataFromTree does not call componentWillUnmount
+        // head side effect therefore need to be cleared manually
+        Head.rewind();
       }
 
       // Extract query data from the Apollo store
@@ -102,21 +120,22 @@ export function withApollo(PageComponent, { ssr = true } = {}) {
   return WithApollo;
 }
 
+let apolloClient = null;
+
 /**
  * Always creates a new apollo client on the server
  * Creates or reuses apollo client in the browser.
- * @param  {Object} initialState
  */
-function initApolloClient(initialState) {
+function initApolloClient(...args) {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (typeof window === 'undefined') {
-    return createApolloClient(initialState);
+    return createApolloClient(...args);
   }
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = createApolloClient(initialState);
+    apolloClient = createApolloClient(...args);
   }
 
   return apolloClient;
@@ -125,16 +144,53 @@ function initApolloClient(initialState) {
 /**
  * Creates and configures the ApolloClient
  * @param  {Object} [initialState={}]
+ * @param  {Object} config
  */
-function createApolloClient(initialState = {}) {
+function createApolloClient(initialState = {}, { getSession }) {
+  const fetchOptions = {};
+
+  // If you are using a https_proxy, add fetchOptions with 'https-proxy-agent' agent instance
+  // 'https-proxy-agent' is required here because it's a sever-side only module
+  if (typeof window === 'undefined') {
+    if (process.env.https_proxy) {
+      fetchOptions.agent = new (require('https-proxy-agent'))(
+        process.env.https_proxy
+      );
+    }
+  }
+
+  const httpLink = new HttpLink({
+    uri: 'https://hhj-jongno-archery-competition.firebaseapp.com/graphql', // Server URL (must be absolute)
+    credentials: 'include',
+    fetch,
+    fetchOptions
+  });
+
+  const authLink = setContext((request, { headers }) => {
+    const session = getSession();
+    return {
+      headers: {
+        ...headers
+        // authorization: token ? `Bearer ${token}` : ''
+      }
+    };
+  });
+
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
   return new ApolloClient({
     ssrMode: typeof window === 'undefined', // Disables forceFetch on the server (so queries are only run once)
-    link: new HttpLink({
-      uri: 'https://hhj-jongno-archery-competition.firebaseapp.com/graphql', // Server URL (must be absolute)
-      credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
-      fetch
-    }),
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache().restore(initialState)
   });
+}
+
+/**
+ * Get the user session from cookie
+ * @param {Object} req
+ */
+function getSession(req) {
+  const cookies = cookie.parse(
+    req ? req.headers.cookie || '' : document.cookie
+  );
+  return cookies.session;
 }
