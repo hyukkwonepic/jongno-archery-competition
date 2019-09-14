f546aafcb5c5aaa3133328d5f3fe670b09ab02a6
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-14 12:03:43 +0900

    Modify component to forward ref

--- web/components/FullTableButton/index.js
+++ web/components/FullTableButton/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ExpandSvg from '../../assets/ExpandSvg';
 
 import * as S from './styles';
 
-export default function FullTableButton(props) {
+const FullTableButton = React.forwardRef(function(props, ref) {
   return (
-    <S.FullTable {...props}>
+    <S.FullTable {...props} ref={ref}>
       <ExpandSvg />
       전체 화면
     </S.FullTable>
   );
-}
+});
+
+export default FullTableButton;
